5cab5e7b0e1ce6efe62ae3ea1024ec714e6419e5
Evgeniy Goncharov <jack38irk@icloud.com>
2023-07-24 20:35:49 +0800

    Initial Commit

---
+++ Avtomobilka/AvtomobilkaApp.swift
@@ -0,0 +1,17 @@
+//
+//  AvtomobilkaApp.swift
+//  Avtomobilka
+//
+//  Created by Evgeniy Goncharov on 24.07.2023.
+//
+
+import SwiftUI
+
+@main
+struct AvtomobilkaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
